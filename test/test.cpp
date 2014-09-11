43425d107fa22c29af48143ae0fe89550c00d9ed
Pierre-Yves Martin <pym.aldebaran@gmail.com>
2014-09-11 12:13:22 +0200

    Add full support for float litteral.

--- test/test.cpp
+++ test/test.cpp
@@ -7,33 +7,76 @@
 // The lexer returns tokens [0-255] if it is an unknown character, otherwise
 // one of these for known things.
 const int TOK_FLOAT = -1;
+const int TOK_EOF   = -2;
 
 // Lexer for the MeeMaw language
 class Lexer {
 private:
     std::istream& stream;   // input stream to parse
     float floatValue;       // Filled in if curTok == TOK_FLOAT
+    int currentToken;       // Current token i.e. the last returned by getNextToken()
+    int lastChar;           // Store the last char read by gettok()
+
+    // gettok - Return the next token from standard input.
+    int gettok();
+
 public:
     // Constructor
-    explicit Lexer(std::istream& strm)
-        : stream(strm), floatValue(0.0)
-    {
-    }
+    explicit Lexer(std::istream& strm);
 
     // floatValue getter
-    float getFloatValue()
-    {
-        //TODO check if curTok == TOK_FLOAT
-        return floatValue;
-    }
+    float getFloatValue();
 
     // Reads another token from the lexer and updates CurTok with its results
-    int getNextToken()
-    {
-        floatValue = 1.0;
+    int getNextToken();
+};
+
+int Lexer::gettok()
+{
+    lastChar = stream.get();
+
+    // Float: [0-9.]+
+    if (isdigit(lastChar) || lastChar == '.') {
+        std::string floatStr;
+
+        do {
+            floatStr += lastChar;
+            lastChar = stream.get();
+        } while (isdigit(lastChar) || lastChar == '.');
+
+        floatValue = strtof(floatStr.c_str(), nullptr);
         return TOK_FLOAT;
     }
-};
+
+    // Check for end of file.  Don't eat the EOF.
+    if (lastChar == EOF)
+        return TOK_EOF;
+
+    // Otherwise, just return the character as its ascii value.
+    int thisChar = lastChar;
+    lastChar = stream.get();
+    return thisChar;
+}
+
+Lexer::Lexer(std::istream& strm) :
+    stream(strm),
+    floatValue(0.0),
+    currentToken(0),
+    lastChar(' ')
+{
+}
+
+float Lexer::getFloatValue()
+{
+    //TODO check if currentToken == TOK_FLOAT
+    return floatValue;
+}
+
+int Lexer::getNextToken()
+{
+    return currentToken = gettok();
+}
+
 
 // TESTS /////////////////////////////////////
 //maximum difference between 2 floats to be considered equal
@@ -73,6 +116,7 @@ TEST_CASE("Lexer categorise float"){
 
     SECTION("Any float between 0.0 and max float is categorized as float"){
         float f = randFloatGenerator();
+
         test << std::to_string(f);
 
         int tokId = lex.getNextToken();
@@ -83,6 +127,7 @@ TEST_CASE("Lexer categorise float"){
 
     SECTION("Random char is not categorized as float"){
         unsigned char c = randUCharGenerator();
+
         test << c;
 
         int tokId = lex.getNextToken();
